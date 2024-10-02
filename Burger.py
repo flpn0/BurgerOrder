b7a3fb76f09a1b5e8a6874066edb125e158e5c86
flpn0 <thewinterburger@gmail.com>
2024-10-02 15:18:48 +0200

    Moved everything around to specific folders to make our css code work with flask, now the website is setup and works perfectly. Now we can begin to work with coding the burger choices and topping choices!

--- Burger.py
+++ Burger.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template
-app = Flask(__name__, static_url_path="Burger.css")
+app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def home():
-    with open('Burger.html', 'r') as fp:
-        html = fp.read()
-    return html
+    return render_template("Burger.html")
 
-@app.route("/buy")
+@app.route("/oder.html")
 def hello_mans():
-    with open('Burger.html', 'r') as fp:
-        html = fp.read()
-    return "kj"
+    return render_template("oder.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)  
