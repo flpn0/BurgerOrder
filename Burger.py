aea56b76105d6a878fe45a9c64b7780c2297ac05
flpn0 <thewinterburger@gmail.com>
2024-10-07 14:56:58 +0200

    Added GET and POST functions to the API so we can take the users order from the website, also added another function that will print out the users order

--- Burger.py
+++ Burger.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+from flask import request
 app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def home():
     return render_template("Burger.html")
 
-@app.route("/oder.html")
-def hello_mans():
+def oder_post():
+    print(request.form)
+    return request.form
+
+@app.route("/oder.html", methods=["GET", "POST"])
+def oder():
+    if request.method == "POST":
+        return oder_post()
     return render_template("oder.html")
 
 if __name__ == "__main__":
