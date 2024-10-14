c542015d288ff0dfa4d5ff53adfd80203a2028f8
flpn0 <thewinterburger@gmail.com>
2024-10-14 17:52:54 +0200

    Finally completed the database, added a gitignore file so not to push local changes with the venv anymore and not push the session database. Also smaller changes like changing names and importing more sqlalchemy functions.

--- KitchenView.py
+++ KitchenView.py
@@ -1,6 +1,38 @@
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, session, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
 app = Flask(__name__, template_folder='templates')
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column("id", Integer, primary_key=True)
+    cheese = Column("cheese", Integer)
+    dressing = Column("dressing", Integer)
+    onion = Column("onion", Integer)
+    patty = Column("patty", Integer)
+    pickles = Column("pickles", Integer)
+
+    def __init__(self, cheese, dressing, onion, patty, pickles):
+        self.cheese = cheese
+        self.dressing = dressing
+        self.onion = onion
+        self.patty = patty
+        self.pickles = pickles
+
+    def __repr__(self):
+        personal_order = f"{self.cheese}, {self.dressing}, {self.onion}, {self.patty}, {self.pickles}"
+        return personal_order
+    
+engine = create_engine("sqlite:///mydb.db", echo=True)
+Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 @app.route("/")
 def home():
@@ -11,10 +43,21 @@ def order_post():
     return request.form
 
 @app.route("/BurgerOrderer.html", methods=["GET", "POST"])
-def oder():
+def order():
     if request.method == "POST":
+
+        cheese = request.form.get("cheese")
+        dressing = request.form.get("dressing")
+        onion = request.form.get("onion")
+        patty = request.form.get("patty")
+        pickles = request.form.get("pickles")
+
+        order = Order(cheese, dressing, onion, patty, pickles)
+        session.add(order)
+        session.commit()
         return order_post()
     return render_template("BurgerOrderer.html")
 
 if __name__ == "__main__":
     app.run(debug=True)  
+    
--- readme.txt
+++ readme.txt
@@ -2,4 +2,4 @@ sudo apt install python3-pip
 pip install Flask
 pip install -r requirements.txt
 source venv/bin/activate
-flask --app Burger.py run
+flask --app KitchenView.py run
